915e238f250da323a02575583a2d412e4cba5f6c
developerDesinger <deisgnercoo@gmail.com>
2026-01-27 16:19:07 +0500

    change

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,23 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   return <>{children}</>;
 };
 
+const PublicRoute = ({ children }: { children: React.ReactNode }) => {
+  const token = localStorage.getItem('admin_token');
+  if (token) {
+    return <Navigate to="/users" replace />;
+  }
+  return <>{children}</>;
+};
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        } />
         
         <Route path="/" element={
           <ProtectedRoute>
